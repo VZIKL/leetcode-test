7d2d6c2e4662ff7c98a1fad49f8bcec45eedc0f5
VZIKL <cvzikl@gmail.com>
2016-11-09 18:11:20 +0800

    twosum

---
+++ two-sum.cpp
@@ -0,0 +1,18 @@
+// File Name: two-sum.cpp
+// Author:
+// Mail:
+// Created Time: 2016年11月09日 星期三 12时16分25秒
+#include <iostream>
+#include <map>
+#include <vector>
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+  int len = nums.size();
+  for (int x = 0; x < len; x++) {
+    for (int y = x + 1; y < len; y++) {
+      if (target == (nums[x] + nums[y])) {
+        return std::vector<int>{x, y};
+      }
+    }
+  }
+  return std::vector<int>();
+}
