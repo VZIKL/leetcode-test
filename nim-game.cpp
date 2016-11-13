345069e49e9fedb883697b97c53d255227903a01
VZIKL <cvzikl@gmail.com>
2016-11-14 03:19:05 +0800

    nim-game

---
+++ nim-game.cpp
@@ -0,0 +1,15 @@
+// File Name: nim-game.cpp
+// Author:
+// Mail:
+// Created Time: 2016年11月14日 星期一 03时17分35秒
+#include <iostream>
+class Soution {
+ public:
+  bool canWinNim(int n) {
+    if (n % 4 == 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
