e4c06b86a7a8b5d99defe8a31efca4378caf0220
VZIKL <cvzikl@gmail.com>
2016-11-14 03:21:36 +0800

    Nim-Game

--- nim-game.cpp
+++ nim-game.cpp
@@ -5,11 +5,5 @@
 #include <iostream>
 class Soution {
  public:
-  bool canWinNim(int n) {
-    if (n % 4 == 0) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+  bool canWinNim(int n) { return n % 4 == 0 ? false : true; }
 };
