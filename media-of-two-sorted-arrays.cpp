d101cb3934cd466a510f7b749fe69a9650794c18
VZIKL <cvzikl@gmail.com>
2016-11-23 14:54:08 +0800

    media-of-two-sorted-arrays

--- media-of-two-sorted-arrays.cpp
+++ media-of-two-sorted-arrays.cpp
@@ -1,16 +1,18 @@
-//File Name: media-of-two-sorted-arrays.cpp
-//Author: 
-//Mail: 
-//Created Time: 2016年11月23日 星期三 21时41分04秒
-#include<iostream>
+// File Name: media-of-two-sorted-arrays.cpp
+// Author:
+// Mail:
+// Created Time: 2016年11月23日 星期三 21时41分04秒
+#include <iostream>
 #include <vector>
 #include <algorithm>
-class Solution{
-    public:
-        double findMedianSortedArrays(std::vector<int>& nums1,std::vector<int>& nums2){
-            std::vector<int> x(nums1.begin(),nums1.end());
-            x.insert(x.end(),nums2.begin(),nums2.end());
-            std::sort(x.begin(),x.end());
-            return x.size()%2 ==0?(x[x.size() / 2] + x[x.size() / 2 -1]) / 2.0:x[x.size()/2];
-        }
+class Solution {
+ public:
+  double findMedianSortedArrays(std::vector<int>& nums1,
+                                std::vector<int>& nums2) {
+    std::vector<int> x(nums1.begin(), nums1.end());
+    x.insert(x.end(), nums2.begin(), nums2.end());
+    std::sort(x.begin(), x.end());
+    return x.size() % 2 == 0 ? (x[x.size() / 2] + x[x.size() / 2 - 1]) / 2.0
+                             : x[x.size() / 2];
+  }
 };
